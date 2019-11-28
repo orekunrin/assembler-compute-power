b27c706f2ddd93f12b2677a179a06e4352239cbe
John Orekunrin <33327768+orekunrin@users.noreply.github.com>
2019-11-28 13:19:33 -0500

    Update COMP375HW1-1.cpp

--- COMP375HW1-1.cpp
+++ COMP375HW1-1.cpp
@@ -1,4 +1,5 @@
 // The purpose of this program is to compute the power of a number
+// Written by John Orekunrin
 
 #include <iostream>
 using namespace std;
@@ -9,16 +10,16 @@ int main()
 	cout << "Enter two numbers, x and n>";
 	cin >> dog >> cat;
 	_asm {	// In assembler set answer to dog^cat
-		mov eax, dog
-		mov ebx, cat
-		again: 
-		   cmp ebx, 1
-		   je finish
-		   dec ebx
-		   imul dog
-		   jmp again
+		mov eax, dog	// move the value of dog into the eax register
+		mov ebx, cat	// move the value of cat into the ebx register
+		again: 		// begins a loop
+		   cmp ebx, 1	// compare the value in the ebx register to 1
+		   je finish	// if the value of ebx equals 1, jump to the line "finish:"
+		   dec ebx	// else, decrement the value in the ebx register
+		   imul dog	// multiply the value in the eax register by dog (imul automatically multiples by whatever value is in the eax register)
+		   jmp again	// jump back to the beginning of the loop
 		finish:
-		   mov answer, eax
+		   mov answer, eax	// when finished, place the value in the eax register to answer
 	}
 	cout << "X to the n is " << answer << endl;
 }
